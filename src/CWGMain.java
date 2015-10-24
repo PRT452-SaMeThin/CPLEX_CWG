f7303a251e84dc3e530e490c2fec5d17b95bec9a
mrinalini90 <mirupikachu@gmail.com>
2015-10-24 20:00:51 +0930

    Main Method is separated

--- src/CWGMain.java
+++ src/CWGMain.java
@@ -3,7 +3,12 @@ public class CWGMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		CWGTestRun test = new CWGTestRun();
+		test.printFileData();
+		test.initialMatix();
+		test.printInputMatrix();
+		test.printFixList();
+		test.model1();
 	}
 	
 
