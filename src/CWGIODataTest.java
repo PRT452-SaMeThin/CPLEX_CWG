2079aa1693ddbd25392ab00b793bbb46fc8b038d
mrinalini90 <mirupikachu@gmail.com>
2015-10-25 04:23:24 +0930

    Unit tests updates

--- src/CWGIODataTest.java
+++ src/CWGIODataTest.java
@@ -27,34 +27,51 @@ public class CWGIODataTest {
 	@Test
 	public void testPrintFileData() {
 					try{
-					            String a="4 4\n0 1\n0 2\n1 2\n2 3\n"; // this values tends to change according to the input
-					            CWGIOData object2 = new CWGIOData();
-					            object2.printFileData();
-					            assertEquals(a,CWGIOData.aString);   
+					          String sourcePath= "/Users/Mrinalini/Desktop/Test1.txt";
+					          FileReader file =new FileReader(sourcePath);
+					          BufferedReader source = new BufferedReader(file);
+					          String a,input="";
+					          while((a=source.readLine())!=null){
+					        					  input= input+a+'\n';
+					        				     }
+					          CWGIOData object2 = new CWGIOData();
+					          object2.printFileData();
+					          assertEquals(input,CWGIOData.aString);   
 					    }catch(Exception e){
-					            System.out.println("Error in testPrintFileData()");
+					           System.out.println("Error in testPrintFileData()");
 					    }
 	}
 
 	@Test
 	public void testInitialMatix() {
-					try{
-				            int numVer = 4;
-				            int input[][] = new int[numVer][numVer];
-				            for (int i = 0; i < numVer; i++) {
-				                for (int j = 0; j < numVer; j++) {
-				                                                 if((i==0)&&(j==3)){input[i][j]=1;}
-				                                            else if((i==1)&&(j==3)){input[i][j]=1;}
-				                                            else if((i==3)&&(j==1)){input[i][j]=1;}
-				                                            else if((i==3)&&(j==0)){input[i][j]=1;}
-				                                            else{input[i][j]=0;}
-				                    }
-				            }
-				            CWGIOData object1 = new CWGIODatan();
-				            object1.initialMatrix();
-				            Assert.assertArrayEquals(input, CWGIOData.X);
-				        }catch(Exception e){
-				            System.out.println("Error in testInitialMatrix()")
+				try{
+					     String sourcePath= "/Users/Mrinalini/Desktop/Test1.txt";
+				             Scanner input = new Scanner(new File(sourcePath));
+				             int vertices = input.nextInt();
+				             int edges= input.nextInt();
+				             int x[][]= new int[edges][edges];
+				             for (int i = 0; i <edges; i++) {
+				        					  x[input.nextInt()][input.nextInt()]=1; 
+				        				     }
+				              for (int p = vertices - 1; p >= 0; p--) {
+				        		  for (int q = p - 1; q>= 0; q--) {
+				        							 x[p][q] = x[q][p];
+				        						 }
+				        					 }
+				              for(int m=0;m<vertices;m++){
+				                	  for(int n =0;n<vertices;n++){
+								                        if(x[m][n]==0)
+								                        	 x[m][n]=1;
+								                        else
+								                        	 x[m][n]=0;
+				                					 }
+				               x[m][m]=0;
+				                }
+				             CWGIOData object1 = new CWGIODatan();
+				             object1.initialMatrix();
+				             Assert.assertArrayEquals(input, CWGIOData.X);
+				  }catch(Exception e){
+				             System.out.println("Error in testInitialMatrix()")
 			        	
 		  }
 }
