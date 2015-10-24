6f99629caf89f7acbf0fd9b12f85698985d1f879
saincogt <saincogt@gmail.com>
2015-10-25 00:37:01 +0930

    20151025 v2 1) Added 4 JUnit tests.

---
+++ src/CWGAlgoTest.java
@@ -0,0 +1,71 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author Sancho
+ *
+ */
+public class CWGAlgoTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link CWGAlgo#printFixList()}.
+	 */
+	@Test
+	public void testPrintFixList() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link CWGAlgo#solveCWG()}.
+	 */
+	@Test
+	public void testSolveCWG() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link CWGAlgo#checkEdgeExist(int, int)}.
+	 */
+	@Test
+	public void testCheckEdgeExist() {
+		fail("Not yet implemented");
+	}
+
+}
